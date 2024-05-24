3f12509084a8f385fc317ccefeb0f97239af4b26
Joaquin <jogimenez@frro.utn.edu.ar>
2024-05-24 14:14:44 -0300

    Servidor Funciona

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import { evento } from './evento.js';
 const app = express();
-app.use('/', (req, res) => {
-    res.send('<h1>MATEO DOWN DE MIERDA</h1>');
+// get -> obtener info sobre recursos
+// get /api/characters/ -> obtener la lsita de characters || get /api/characters/:id -> obtener el character con id = :id
+//
+// post -> crear nuevos recursos 
+// post /api/characters/ -> crea nuevo character
+//
+// delete -> borrar recursos
+// delete /api/characters/:id -> borrar character con id = :id
+//
+//put & patch -> modificar recursos
+//put & patch /api/characters/:id -> modifica character con id = :id
+//
+// character -> /api/characters/
+const unEvento = [
+    new evento(123123, 'Techno', 3000, 'EVENTO GAY ADMIN: MATEO SEFFINO', 123, 'no se cuando meirda es', 20),
+];
+app.get('/api/eventos', (req, res) => {
+    res.json(unEvento);
 });
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000/');
---
+++ src/evento.ts
@@ -0,0 +1,11 @@
+export class evento{
+    constructor(
+        public idEvento:number,
+        public nombre:string,
+        public cuposGral:number,
+        public descripcion:string,
+        public fotoEvento:number,
+        public fecha:string,
+        public hora:number,
+    ) {}
+}
