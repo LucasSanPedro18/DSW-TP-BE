cb1e75841a57d6f1ea65e2ca6bd5bd76d72233d7
Joaquin <jogimenez@frro.utn.edu.ar>
2024-06-13 20:03:17 -0300

    todo ok

--- dist/app.js
+++ dist/app.js
@@ -52,31 +52,68 @@ const usuarios = [
     new usuario('20202020202020', 1, 0),
 ];
 app.get('/api/eventos', (req, res) => {
-    res.json(eventos);
+    return res.json(eventos);
 });
 app.get('/api/eventos/:id', (req, res) => {
     const nuevoEvento = eventos.find((evento) => evento.idEvento === Number(req.params.id));
     if (!nuevoEvento) {
-        res.status(404).send({ message: 'evento no encontrado' });
+        return res.status(404).send({ message: 'evento no encontrado' });
     }
-    res.json(nuevoEvento);
+    return res.json(nuevoEvento);
 });
-app.post('/api/eventos', (req, res) => {
+app.post('/api/eventos', sanitizeEventoInput, (req, res) => {
     //req.body donde se encuentra la informacion del post
-    const { idEvento, nombre, cuposGral, descripcion, fotoEvento, fecha, hora } = req.body;
-    const nuevoEvento = new evento(idEvento, nombre, cuposGral, descripcion, fotoEvento, fecha, hora);
+    const input = req.body.sanitizedInput;
+    const nuevoEvento = new evento(input.idEvento, input.nombre, input.cuposGral, input.descripcion, input.fotoEvento, input.fecha, input.hora);
     eventos.push(nuevoEvento);
-    res.status(201).send({ message: 'Evento creado', data: nuevoEvento });
+    return res.status(201).send({ message: 'Evento creado', data: nuevoEvento });
 });
-app.get('/api/categorias', (req, res) => {
-    res.json(categorias);
+app.put('/api/eventos/:id', sanitizeEventoInput, (req, res) => {
+    const eventoIDx = eventos.findIndex((evento) => evento.idEvento === Number(req.params.id));
+    if (eventoIDx === -1) {
+        return res.status(404).send({ message: 'evento no encontrado' });
+    }
+    eventos[eventoIDx] = { ...eventos[eventoIDx], ...req.body.sanitizedInput };
+    return res.status(200).send({ message: 'Evento actualizado correctamente', data: eventos[eventoIDx] });
 });
-app.get('/api/lasdosjuntas', (req, res) => {
-    const lasdosjuntas = {
-        evento: eventos,
-        categoria: categorias,
+function sanitizeEventoInput(req, res, next) {
+    req.body.sanitizedInput = {
+        idEvento: req.body.idEvento,
+        nombre: req.body.nombre,
+        cuposGral: req.body.cuposGral,
+        descripcion: req.body.descripcion,
+        fotoEvento: req.body.fotoEvento,
+        fecha: req.body.fecha,
+        hora: req.body.hora,
     };
-    res.json(lasdosjuntas);
+    //validar tipo de datos, etc etc etc etc etc etc etc
+    Object.keys(req.body.sanitizedInput).forEach((key) => {
+        if (req.body.sanitizedInput[key] === undefined) {
+            delete req.body.sanitizedInput[key];
+        }
+    });
+    next();
+}
+app.patch('/api/eventos/:id', sanitizeEventoInput, (req, res) => {
+    const eventoIDx = eventos.findIndex((evento) => evento.idEvento === Number(req.params.id));
+    if (eventoIDx === -1) {
+        return res.status(404).send({ message: 'evento no encontrado' });
+    }
+    Object.assign(eventos[eventoIDx], req.body.sanitizedInput);
+    return res.status(200).send({ message: 'Evento actualizado correctamente', data: eventos[eventoIDx] });
+});
+app.delete('/api/eventos/:id', (req, res) => {
+    const eventoIDx = eventos.findIndex((evento) => evento.idEvento === Number(req.params.id));
+    if (eventoIDx === -1) {
+        res.status(404).send({ message: 'Evento no encontrado' });
+    }
+    else {
+        eventos.splice(eventoIDx, 1);
+        res.status(200).send({ message: 'Evento borrado satisfactoriamente' });
+    }
+});
+app.use((req, res) => {
+    res.status(404).send({ message: 'Recurso no encontrado' });
 });
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000/');
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { evento } from './evento.js'
 import { categoria } from './categoria.js'
 import { cuenta } from './cuenta.js'
@@ -105,48 +105,85 @@ const usuarios = [
 ]
 
 app.get('/api/eventos',(req,res)=>{
-    res.json(eventos)
+    return res.json(eventos)
 })
 
 app.get('/api/eventos/:id',(req,res)=>{
     const nuevoEvento = eventos.find((evento)=>evento.idEvento===Number(req.params.id)) 
     if (!nuevoEvento) {
-        res.status(404).send({message: 'evento no encontrado'})
+        return res.status(404).send({message: 'evento no encontrado'})
     }
-    res.json(nuevoEvento)
+    return res.json(nuevoEvento)
 })  
 
-app.post('/api/eventos',(req,res)=>{
+app.post('/api/eventos', sanitizeEventoInput, (req,res)=>{
     //req.body donde se encuentra la informacion del post
-    const {idEvento, nombre, cuposGral, descripcion, fotoEvento, fecha, hora}= req.body
+    const input = req.body.sanitizedInput
     const nuevoEvento = new evento(
-        idEvento,
-        nombre, 
-        cuposGral, 
-        descripcion, 
-        fotoEvento, 
-        fecha, 
-        hora
+        input.idEvento,
+        input.nombre, 
+        input.cuposGral, 
+        input.descripcion, 
+        input.fotoEvento, 
+        input.fecha, 
+        input.hora
     )
     eventos.push(nuevoEvento)
-    res.status(201).send({message: 'Evento creado', data: nuevoEvento})
+    return res.status(201).send({message: 'Evento creado', data: nuevoEvento})
 })
 
-
-
-
-app.get('/api/categorias',(req,res)=>{
-    res.json(categorias)
-})
-
-app.get('/api/lasdosjuntas',(req,res)=>{
-    const lasdosjuntas = {
-        evento: eventos,
-        categoria: categorias,
+app.put('/api/eventos/:id', sanitizeEventoInput, (req, res)=>{
+    const eventoIDx = eventos.findIndex((evento) => evento.idEvento === Number(req.params.id))
+    if(eventoIDx === -1){
+        return res.status(404).send({message: 'evento no encontrado'})
     }
-    res.json(lasdosjuntas)
+    eventos[eventoIDx] = { ...eventos[eventoIDx], ...req.body.sanitizedInput}
+    return res.status(200).send({message:'Evento actualizado correctamente', data: eventos[eventoIDx]})
 })
 
+function sanitizeEventoInput(req: Request, res: Response, next: NextFunction){
+
+    req.body.sanitizedInput = { 
+        idEvento : req.body.idEvento,
+        nombre : req.body.nombre,
+        cuposGral : req.body.cuposGral,
+        descripcion : req.body.descripcion,
+        fotoEvento : req.body.fotoEvento,
+        fecha : req.body.fecha,
+        hora : req.body.hora,
+    };
+    //validar tipo de datos, etc etc etc etc etc etc etc
+
+    Object.keys(req.body.sanitizedInput).forEach((key) => {
+        if (req.body.sanitizedInput[key] === undefined) {
+            delete req.body.sanitizedInput[key];
+        }
+    });
+    next();
+}
+
+app.patch('/api/eventos/:id', sanitizeEventoInput, (req, res)=>{
+    const eventoIDx = eventos.findIndex((evento) => evento.idEvento === Number(req.params.id))
+    if(eventoIDx === -1){
+        return res.status(404).send({message: 'evento no encontrado'})
+    } 
+    Object.assign (eventos[eventoIDx],req.body.sanitizedInput)
+    return res.status(200).send({message:'Evento actualizado correctamente', data: eventos[eventoIDx]})
+})
+
+app.delete('/api/eventos/:id', (req, res) => {
+    const eventoIDx = eventos.findIndex((evento) => evento.idEvento === Number(req.params.id))
+    if(eventoIDx === -1) {
+        res.status(404).send({message:'Evento no encontrado'})
+    }else {
+        eventos.splice(eventoIDx, 1)
+        res.status(200).send({message: 'Evento borrado satisfactoriamente'})
+    }
+})
+
+app.use((req, res) => {
+    res.status(404).send({message:'Recurso no encontrado'})
+})
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000/')
