0780f955f3668db6d4f78b325de7d074c2d37fe1
JoaquinGZ1 <jogimenez@frro.utn.edu.ar>
2024-08-31 19:56:13 -0300

    Create mysql-commands.sql

---
+++ docs/mysql-commands.sql
@@ -0,0 +1,31 @@
+create database if not exists GEEV;
+
+use GEEV;
+
+## uncomment if you are not using docker
+## create user if not exists dsw@'%' identified by 'dsw';
+## grant select, update, insert, delete on GEEV.* to dsw@'%';
+
+
+create table if not exists `GEEV`.`eventos` (
+  `idEvento` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NULL,
+  `cuposGral` INT UNSIGNED NULL,
+  `descripcion` VARCHAR(255) NULL,
+  `fotoEvento` IMAGE UNSIGNED NULL,
+  `fecha` DATE UNSIGNED NULL,
+  `hora` INT UNSIGNED NULL,
+  PRIMARY KEY (`id`));
+
+create table if not exists `GEEV`.`eventoCategoria` (
+  `characterId` INT UNSIGNED NOT NULL,
+  `categoriaNombre` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`characterId`, `itemName`),
+  CONSTRAINT `fk_characterItem_character`
+    FOREIGN KEY (`characterId`)
+    REFERENCES `eventos`.`characters` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE);
+
+insert into eventos.eventos values(1,'festichola',200,'que se yo',101,11,22);
+insert into eventos.eventoCategoria values(1,'Techno');
